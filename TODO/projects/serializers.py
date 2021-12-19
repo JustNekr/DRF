0751ec3b90afe02ee5628fd36eb9307d4a6646c7
JustNekr <justnekr@gmail.com>
2021-12-19 15:37:14 +0200

    lesson_3

---
+++ TODO/projects/serializers.py
@@ -0,0 +1,19 @@
+from rest_framework import serializers
+from .models import Project, TODO
+
+
+class ProjectSerializer(serializers.ModelSerializer):
+    users = serializers.StringRelatedField(many=True)
+
+    class Meta:
+        model = Project
+        fields = '__all__'
+
+
+class TODOSerializer(serializers.ModelSerializer):
+    project = serializers.HyperlinkedRelatedField(read_only=True, view_name='project')
+    users = serializers.StringRelatedField(many=True)
+
+    class Meta:
+        model = TODO
+        fields = '__all__'
