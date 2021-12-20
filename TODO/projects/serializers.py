ba4b37d621da4149cdb0a1667614fd5dfa3f64cc
JustNekr <justnekr@gmail.com>
2021-12-20 16:32:35 +0200

    lesson_4

--- TODO/projects/serializers.py
+++ TODO/projects/serializers.py
@@ -5,7 +5,6 @@ from .models import Project, TODO
 
 
 class ProjectSerializer(serializers.ModelSerializer):
-    # users = serializers.StringRelatedField(many=True)
     users = serializers.StringRelatedField(read_only=True, many=True)
 
     class Meta:
