2fe1318efa590a62165f735e465ce28497cb2789
JustNekr <justnekr@gmail.com>
2021-12-27 11:37:52 +0200

    lesson_5

--- TODO/frontend/src/App.js
+++ TODO/frontend/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
+import {BrowserRouter as Router, Routes, Route, Link} from "react-router-dom";
 import './App.css';
-import UserList from './components/User.js'
 import axios from 'axios'
+
+import UserList from './components/User.js'
 import Menu from "./components/Menu";
 import Footer from "./components/Footer";
+import ProjectList from './components/Project.js'
 
 
 
@@ -13,17 +15,28 @@ class App extends React.Component {
    constructor(props) {
        super(props)
        this.state = {
-           'users': []
+           'users': [],
+           'projects': []
        }
    }
 
    componentDidMount() {
        axios.get('http://127.0.0.1:8000/api/users')
            .then(response => {
-               const users = response.data
+               const users = response.data.results
                this.setState(
                    {
                        'users': users
+                   }
+               )
+           }).catch(error => console.log(error))
+
+       axios.get('http://127.0.0.1:8000/api/projects')
+           .then(response => {
+               const projects = response.data.results
+               this.setState(
+                   {
+                       'projects': projects
                    }
                )
            }).catch(error => console.log(error))
@@ -33,7 +46,25 @@ class App extends React.Component {
        return (
            <div>
                <Menu menu={'Menu'}/>
-               <UserList users={this.state.users} />
+               <Router>
+                    <nav>
+                      <ul>
+                        <li>
+                          <Link to="/projects">Projects</Link>
+                        </li>
+                        <li>
+                          <Link to="/">Users</Link>
+                        </li>
+                      </ul>
+                    </nav>
+                    {/* A <Switch> looks through its children <Route>s and
+                        renders the first one that matches the current URL. */}
+                    <Routes>
+                        <Route path="/" element={<UserList users={this.state.users} />} />
+                        <Route path="/projects" element={<ProjectList projects={this.state.projects}/>} />
+                    </Routes>
+                </Router>
+
                <Footer text={'Footer'}/>
            </div>
        )
